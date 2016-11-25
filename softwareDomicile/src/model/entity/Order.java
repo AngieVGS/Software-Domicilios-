21f232868b8aa22e078744a5b816b5a33ba288f9
AngieVGS <pativivi203@gmail.com>
2016-11-24 21:10:36 -0500

    agregue los atributos a la clase Order y cree el constructor

--- softwareDomicile/src/model/entity/Order.java
+++ softwareDomicile/src/model/entity/Order.java
@@ -1,5 +1,23 @@
 package model.entity;
 
+import java.util.ArrayList;
+
 public class Order {
 
+	private int id;
+	private ArrayList<Product> productsOfTheOrder;
+	private State state;
+	private String direction;
+	
+	public Order(int id, ArrayList<Product> productsOfTheOrder, State state, String direction) {
+		this.id = id;
+		this.productsOfTheOrder = productsOfTheOrder;
+		this.state = state;
+		this.direction = direction;
+	}
+	
+	
+	public void addproduct(Product product){
+		
+	}
 }
