7f0af69f358e48c7aec001a12f6f2253f7f458e1
Lorespitia24 <lorespitia24@gmail.com>
2016-11-24 20:42:45 -0500

    esqueleto

---
+++ softwareDomicile/src/controller/Controller.java
@@ -0,0 +1,15 @@
+package controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Controller implements ActionListener{
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		switch (Actions.valueOf(e.getActionCommand())) {
+		}
+		
+	}
+
+}
