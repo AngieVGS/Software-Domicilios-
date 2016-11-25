e418501d6708371a8128fb46b86693acaa33975c
Lorespitia24 <lorespitia24@gmail.com>
2016-11-24 23:35:07 -0500

    Agregue el constructor del dueño

--- softwareDomicile/src/model/entity/Owner.java
+++ softwareDomicile/src/model/entity/Owner.java
@@ -1,5 +1,62 @@
 package model.entity;
 
+import java.util.ArrayList;
+import exceptions.ExceptionSearchId;
+
 public class Owner {
 
+	private int id;
+	private String name;
+	private String password;
+	private ArrayList<Product> productList;
+	
+	public Owner(int id, String name, String password, ArrayList<Product> productList) {
+		this.id = id;
+		this.name = name;
+		this.password = password;
+		this.productList = productList;
+	}
+	
+//	public static Product createProduct(int id, String name, String description, double price, State state) {
+//		return new Product(id, name, description, price, state);
+//	}
+	
+	public void addProduct(Product product) {
+		productList.add(product);
+	}
+	
+	public void addProducts(ArrayList<Product> products) {
+		for (Product product : products) {
+			productList.add(product);
+		}
+	}
+	
+	public Product searchProductById(int id) throws ExceptionSearchId {
+		for (Product product : productList) {
+//			if (product.getId() == id) {
+				return product;
+//			}
+		}
+		throw new ExceptionSearchId(); 
+	}
+	
+	public ArrayList<Product> searchProductByName(String name) {
+		ArrayList<Product> productSearchList = new ArrayList<>();
+		for (Product product : productList) {
+//			if (product.getName().equalsIgnoreCase(name)) {
+				productSearchList.add(product);
+//			}
+		}
+		return productSearchList;
+	}
+	
+	public void edit(Product product) throws ExceptionSearchId {
+		for (int i = 0; i < productList.size(); i++) {
+//			if (productList.get(i).getId() == product.getId()) {
+				productList.remove(i);
+				productList.add(i, product); 
+				break;
+//			}
+		}
+	}
 }
