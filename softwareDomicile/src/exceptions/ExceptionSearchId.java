cd5cc3f1cbf3e6fca639292ba9adb3378f58d956
Lorespitia24 <lorespitia24@gmail.com>
2016-11-24 23:21:57 -0500

    cree la excepcion de buscar id

---
+++ softwareDomicile/src/exceptions/ExceptionSearchId.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+public class ExceptionSearchId extends Exception{
+
+	private static final long serialVersionUID = 1L;
+
+	public ExceptionSearchId() {
+		super("The monster you are looking for does not exist.");
+	}
+}
