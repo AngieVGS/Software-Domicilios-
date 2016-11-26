0e246b6e9a7bea6065310ab854794657d69b9ca8
Miller Reina <miller-reina@hotmail.com>
2016-11-25 23:17:34 -0500

    Revisen la clase orden, ¿por que hay dos metodos retornando null?

--- softwareDomicile/src/model/entity/Order.java
+++ softwareDomicile/src/model/entity/Order.java
@@ -16,18 +16,15 @@ public class Order {
 		this.direction = direction;
 	}
 	
-	
-	
 	public void addproduct(Product product){
-		
+		productsOfTheOrder.add(product);
 	}
-
+	
+	// Para que estos metodos? no hay si quiera un atributo nombre y encima se estan usando en la clase Usuario
 	public int getId() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-
-
 
 	public String getName() {
 		// TODO Auto-generated method stub
