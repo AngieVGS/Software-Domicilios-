45c03a164ee4ca207ef1f18448fa7011ba2e0d2b
JOSE-PC <JOSE-PC@DESKTOP-2MUEOC4.uptc.edu.co>
2016-11-26 20:19:33 -0500

    cambie los metodos del producto de la clase owner a el manager

--- softwareDomicile/src/model/entity/Owner.java
+++ softwareDomicile/src/model/entity/Owner.java
@@ -1,75 +1,23 @@
 package model.entity;
 
-import java.util.ArrayList;
-import exceptions.ExceptionSearchId;
 
 public class Owner {
 
 	private int id;
 	private String name;
 	private String password;
-	private ArrayList<Product> productList;
+
 	
-	public Owner(int id, String name, String password, ArrayList<Product> productList) {
+	public Owner(int id, String name, String password) {
 		this.id = id;
 		this.name = name;
 		this.password = password;
-		this.productList = productList;
-	}
 	
-	public static Product createProduct(int id, String name, String description, double price, State state) {
-		return new Product(id, name, description, price, state);
 	}
 	
 	public int getId(){
 		return id;
 	}
 	
-	public void addProduct(Product product) {
-		productList.add(product);
-	}
 	
-	public void addProducts(ArrayList<Product> products) {
-		for (Product product : products) {
-			productList.add(product);
-		}
-	}
-	
-	public Product searchProductById(int id) throws ExceptionSearchId {
-		for (Product product : productList) {
-			if (product.getId() == id) {
-				return product;
-			}
-		}
-		throw new ExceptionSearchId(); 
-	}
-	
-	public ArrayList<Product> searchProductByName(String name) {
-		ArrayList<Product> productSearchList = new ArrayList<>();
-		for (Product product : productList) {
-			if (product.getName().equalsIgnoreCase(name)) {
-				productSearchList.add(product);
-			}
-		}
-		return productSearchList;
-	}
-	
-	public void editProduct(Product product) throws ExceptionSearchId {
-		for (int i = 0; i < productList.size(); i++) {
-			if (productList.get(i).getId() == product.getId()) {
-				productList.remove(i);
-				productList.add(i, product); 
-				break;
-			}
-		}
-	}
-	
-	public void deleteProduct(int id) {
-		for (Product product : productList) {
-			if (product.getId() == id) {
-				productList.remove(product);
-				break;
-			}
-		}
-	}
 }
