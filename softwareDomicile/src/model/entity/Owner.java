a525d4a88d22628b93f011f2a3dd144a63a93081
joan fonseca <joanfv111@gmail.com>
2016-11-28 21:33:30 -0500

    organizando

--- softwareDomicile/src/model/entity/Owner.java
+++ softwareDomicile/src/model/entity/Owner.java
@@ -1,23 +1,18 @@
 package model.entity;
 
-
 public class Owner {
 
 	private int id;
 	private String name;
 	private String password;
 
-	
 	public Owner(int id, String name, String password) {
 		this.id = id;
 		this.name = name;
 		this.password = password;
-	
 	}
-	
-	public int getId(){
+
+	public int getId() {
 		return id;
 	}
-	
-	
 }
