f1c0021bf6e4d8322bc8028cf0093b46889c1eb3
Carlos <carlos.rodriguez12@uptc.edu.co>
2016-11-29 09:37:08 -0500

    importé las clases de manager respectivamente al controller

--- softwareDomicile/src/controller/Controller.java
+++ softwareDomicile/src/controller/Controller.java
@@ -2,7 +2,9 @@ package controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import model.dao.Manager;
+import model.dao.OwnerManager;
+import model.dao.ProductManager;
+import model.dao.UserManager;
 import model.entity.Owner;
 import model.entity.User;
 import view.MainWindow;
@@ -10,7 +12,9 @@ import view.MainWindow;
 public class Controller implements ActionListener{
 	
 	private MainWindow mainWindow;
-	private Manager manager;
+	private OwnerManager ownerManager;
+	private ProductManager productManager;
+	private UserManager userManager;
 	private User user;
 	private Owner owner;
 	
