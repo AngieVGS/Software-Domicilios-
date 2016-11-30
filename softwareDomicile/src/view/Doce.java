718edc21b30b14a44710f010f8442139567dc82f
Lorespitia24 <lorespitia24@gmail.com>
2016-11-30 14:00:25 -0500

    agregue ventana dos y la de producto

---
+++ softwareDomicile/src/view/Doce.java
@@ -0,0 +1,171 @@
+package view;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Image;
+import java.awt.Insets;
+import java.awt.Toolkit;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.awt.dnd.DnDConstants;
+import java.awt.dnd.DropTarget;
+import java.awt.dnd.DropTargetDropEvent;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.IOException;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFormattedTextField;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import controller.Controller;
+
+public class Doce extends JDialog{
+
+	private static final long serialVersionUID = 1L;
+
+	private String imageDefault;
+	private JLabel lblTitle, lblImage;
+	private JTextField txtNameProduct;
+	private JTextArea txtDescription;
+	private JFormattedTextField ftxtPrice;
+	private JButton btnCreateProduct;
+
+	public Doce(Controller controller, MainWindow mainWindow) {
+		super(mainWindow, true);
+		setSize(410, 720);
+		setLocationRelativeTo(null);
+		setLayout(new GridBagLayout());
+		imageDefault = ConstantsUI.IMAGE_DEFAULT;
+		setUndecorated(true);
+		getContentPane().setBackground(ConstantsUI.BACKGROUND_COLOR);
+
+		GridBagConstraints gbc = new GridBagConstraints();
+
+		lblTitle = new JLabel("CREATE PRODUCT");
+		lblTitle.setForeground(Color.WHITE);
+		lblTitle.setFont(new Font("Arial Narrow", Font.BOLD, 40));
+		gbc.insets = new Insets(15,40,15,40);
+		gbc.weightx = 1.5;
+		gbc.gridwidth = 2;
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.fill = GridBagConstraints.CENTER;
+		add(lblTitle, gbc);
+		
+		lblImage = new JLabel();
+		gbc.gridy = 1;
+		lblImage.setIcon(new ImageIcon(imageDefault));
+		lblImage.setPreferredSize(new Dimension(128, 128));
+		lblImage.setDropTarget(new DropTarget(){
+			private static final long serialVersionUID = 1L;
+			public synchronized void drop(DropTargetDropEvent dtde) {
+				dtde.acceptDrop(DnDConstants.ACTION_COPY);
+				try {
+					addImage(dtde.getTransferable().getTransferData(DataFlavor.javaFileListFlavor).toString().replace("[", "").replace("]", "").replace("\\", "/"));
+					Toolkit.getDefaultToolkit().beep();
+				} catch (UnsupportedFlavorException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		add(lblImage, gbc);
+		
+		txtNameProduct = new JTextField(ConstantsUI.PRODUCT_NAME);
+		txtNameProduct.setBackground(ConstantsUI.BACKGROUND_COLOR_TEXTFILE);
+		txtNameProduct.setBorder(null);
+		txtNameProduct.addFocusListener(new FocusListener() {
+        	public void focusGained(FocusEvent e) {
+        		if (txtNameProduct.getText().equals(ConstantsUI.PRODUCT_NAME)) {
+        			txtNameProduct.setText("");
+        		}
+        	}
+        	public void focusLost(FocusEvent e) {
+//        		txPassword.setText("Password");
+        	}
+        });
+		txtNameProduct.setForeground(ConstantsUI.BACKGROUND_COLOR);
+		txtNameProduct.setFont(new Font("Arial", Font.PLAIN, 15)); 
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		gbc.ipady = 20;
+		gbc.gridy = 2;
+		add(txtNameProduct, gbc);
+		
+		txtDescription = new JTextArea(ConstantsUI.DESCRIPTION_PRODUCT);
+		txtDescription.setLineWrap(true);//corta la linea
+		txtDescription.setRows(10);//la canttidad de columnas que muestra
+		txtDescription.setBackground(ConstantsUI.BACKGROUND_COLOR_TEXTFILE);
+		txtDescription.setBorder(null);
+		txtDescription.setForeground(ConstantsUI.BACKGROUND_COLOR);
+		txtDescription.addFocusListener(new FocusListener() {
+        	public void focusGained(FocusEvent e) {
+        		if (txtDescription.getText().equals(ConstantsUI.DESCRIPTION_PRODUCT)) {
+        			txtDescription.setText("");
+        		}
+        	}
+        	public void focusLost(FocusEvent e) {
+//        		txPassword.setText("Password");
+        	}
+        });
+		txtDescription.setFont(new Font("Arial", Font.PLAIN, 15)); 
+		JScrollPane jScrollPane = new JScrollPane(txtDescription);
+		jScrollPane.setBorder(null);
+		jScrollPane.setBackground(Color.WHITE);
+		gbc.gridy = 3;
+		add(jScrollPane, gbc);
+		
+		ftxtPrice = new JFormattedTextField(ConstantsUI.getIntegerFormatter());
+		ftxtPrice.setValue(0);
+		ftxtPrice.setBackground(ConstantsUI.BACKGROUND_COLOR_TEXTFILE);
+		ftxtPrice.setBorder(null);
+		ftxtPrice.setForeground(ConstantsUI.BACKGROUND_COLOR);
+		ftxtPrice.setFont(new Font("Arial", Font.PLAIN, 15)); 
+		gbc.gridy = 4;
+		add(ftxtPrice, gbc);
+		
+		btnCreateProduct = new JButton(ConstantsUI.BUTTON_CREATE_PRODUCT);
+		btnCreateProduct.setBorder(null);
+//		btnCreateProduct.setContentAreaFilled(false);
+		btnCreateProduct.setBackground(ConstantsUI.BACKGROUND_COLOR_BUTTON);
+		btnCreateProduct.setForeground(Color.WHITE);
+		btnCreateProduct.setFont(new Font("Franklin Gothic Heavy", Font.BOLD, 15));
+		btnCreateProduct.addActionListener(controller);
+//		btnJoin.setActionCommand(Actions.CREATE_ACCOUNT_OWNER.toString());
+		gbc.insets = new Insets(15,150,15,100);
+		gbc.ipady = 15;
+		gbc.gridy = 5;
+		add(btnCreateProduct, gbc);
+		
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				super.windowClosing(e);
+				clear();
+			}
+		});
+	}
+	
+	public void addImage(String pathImg) {
+		imageDefault = pathImg;
+		Image img = new ImageIcon(imageDefault).getImage().getScaledInstance(128, 118, java.awt.Image.SCALE_AREA_AVERAGING);
+		lblImage.setIcon(new ImageIcon(img));
+	}
+	
+	public void clear() {
+		txtNameProduct.setText(ConstantsUI.PRODUCT_NAME);
+		txtDescription.setText(ConstantsUI.DESCRIPTION_PRODUCT);
+		ftxtPrice.setValue(0);
+	}
+}
---
+++ softwareDomicile/src/view/Dos.java
@@ -0,0 +1,180 @@
+package view;
+
+import javax.swing.JDialog;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFormattedTextField;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+import controller.Controller;
+
+public class Dos extends JDialog{
+
+	private static final long serialVersionUID = 1L;
+
+		private JLabel lblTitle;
+		private JButton btnJoin;
+		private JTextField txfUserName;
+		private JPasswordField txPassword, txConfirmPasword;
+		private JFormattedTextField ftxfContactNumber;
+		
+		public Dos(Controller controller, MainWindow mainWindow) {
+			super(mainWindow, true);
+			setSize(410, 720);
+			setLocationRelativeTo(null);
+			setLayout(new GridBagLayout());
+			setUndecorated(true);
+			getContentPane().setBackground(ConstantsUI.BACKGROUND_COLOR);
+			
+			GridBagConstraints gbc = new GridBagConstraints();
+			
+			lblTitle = new JLabel("CREATE ACCOUNT");
+			lblTitle.setForeground(Color.WHITE);
+			lblTitle.setFont(new Font("Arial Narrow", Font.BOLD, 40));
+			gbc.insets = new Insets(15,40,15,40);
+			gbc.weightx = 1.5;
+			gbc.gridwidth = 2;
+			gbc.gridx = 0;
+	        gbc.gridy = 0;
+	        gbc.fill = GridBagConstraints.CENTER;
+			add(lblTitle, gbc);
+			
+			gbc.gridy = 1;
+			add(new JLabel(), gbc);
+			
+			gbc.insets = new Insets(15,-15,15,0);
+			gbc.anchor = GridBagConstraints.CENTER;
+			gbc.gridwidth = 1;
+			gbc.gridx = 0;
+			gbc.gridy = 2;
+			add(new JLabel(new ImageIcon("src/image/UserIcon.JPG")), gbc);
+			
+	        gbc.gridy = 3;
+			add(new JLabel(new ImageIcon("src/image/password.JPG")), gbc);
+			
+			gbc.gridy = 4;
+			add(new JLabel(new ImageIcon("src/image/password.JPG")), gbc);
+			
+			gbc.gridy = 5;
+			add(new JLabel(new ImageIcon("src/image/Phone.JPG")), gbc);
+			
+			txfUserName = new JTextField(ConstantsUI.USER_NAME);
+			txfUserName.setBackground(ConstantsUI.BACKGROUND_COLOR_TEXTFILE);
+			txfUserName.setBorder(null);
+			txfUserName.setForeground(ConstantsUI.BACKGROUND_COLOR);
+			txfUserName.setFont(new Font("Arial", Font.PLAIN, 15));
+			gbc.fill = GridBagConstraints.HORIZONTAL;
+			gbc.insets = new Insets(15,-60,15,40);
+//			gbc.gridwidth = 2;
+			gbc.ipady = 20;
+			gbc.gridx = 1;
+			gbc.gridy = 2;
+			txfUserName.addFocusListener(new FocusListener() {
+				public void focusGained(FocusEvent e) {
+					if (txfUserName.getText().equals(ConstantsUI.USER_NAME)) {
+						txfUserName.setText("");
+					}
+				}
+				public void focusLost(FocusEvent e) {
+					
+//	        		txfRestaurantName.setText("Restaurant Name");
+				}
+			});
+			add(txfUserName, gbc);
+			
+			txPassword = new JPasswordField(ConstantsUI.PASSWORD);
+			txPassword.setBackground(ConstantsUI.BACKGROUND_COLOR_TEXTFILE);
+			txPassword.setBorder(null);
+			txPassword.setForeground(ConstantsUI.BACKGROUND_COLOR);
+			txPassword.setFont(new Font("Arial", Font.PLAIN, 15)); 
+			gbc.gridx = 1;
+	        gbc.gridy = 3;
+	        txPassword.addFocusListener(new FocusListener() {
+	        	public void focusGained(FocusEvent e) {
+	        		if (txPassword.getPassword().equals(ConstantsUI.PASSWORD)) {
+	        			txPassword.setText("");
+	        		}
+	        	}
+	        	public void focusLost(FocusEvent e) {
+//	        		txPassword.setText("Password");
+	        	}
+	        });
+			add(txPassword, gbc);
+			
+			
+			txConfirmPasword = new JPasswordField(ConstantsUI.PASSWORD);
+			txConfirmPasword.setBackground(ConstantsUI.BACKGROUND_COLOR_TEXTFILE);
+			txConfirmPasword.setBorder(null);
+			txConfirmPasword.setForeground(ConstantsUI.BACKGROUND_COLOR);
+			txConfirmPasword.setFont(new Font("Arial", Font.PLAIN, 15)); 
+			gbc.gridy = 4;
+			txConfirmPasword.addFocusListener(new FocusListener() {
+				public void focusGained(FocusEvent e) {
+					if (txConfirmPasword.getPassword().equals(ConstantsUI.PASSWORD.chars())) {
+						txConfirmPasword.setText("");
+					}
+				}
+				
+				public void focusLost(FocusEvent e) {
+//					txConfirmPasword.setText("Password");
+				}
+			});
+			add(txConfirmPasword, gbc);
+			
+			ftxfContactNumber = new JFormattedTextField(ConstantsUI.PHONE_NUMBER);
+			ftxfContactNumber.setBackground(ConstantsUI.BACKGROUND_COLOR_TEXTFILE);
+			ftxfContactNumber.setBorder(null);
+			ftxfContactNumber.setForeground(ConstantsUI.BACKGROUND_COLOR);
+			ftxfContactNumber.setFont(new Font("Arial", Font.PLAIN, 15)); 
+			gbc.gridy = 5;
+			ftxfContactNumber.addFocusListener(new FocusListener() {
+				public void focusGained(FocusEvent e) {
+					if (ftxfContactNumber.getText().equals(ConstantsUI.PHONE_NUMBER)) {
+					ftxfContactNumber.setText("");
+					}
+				}
+				public void focusLost(FocusEvent e) {
+//					ftxfContactNumber.setText("Phone");
+				}
+			});
+			add(ftxfContactNumber, gbc);
+			
+			btnJoin = new JButton(new ImageIcon("src/image/Buttons/Joinbutton.png"));
+			btnJoin.setBorder(null);
+			btnJoin.setContentAreaFilled(false);
+			btnJoin.addActionListener(controller);
+//			btnJoin.setActionCommand(Actions.CREATE_ACCOUNT_OWNER.toString());
+			gbc.ipady = 20;
+			gbc.gridy = 6;
+			add(btnJoin, gbc);
+			
+			addWindowListener(new WindowAdapter() {
+				@Override
+				public void windowClosing(WindowEvent e) {
+					super.windowClosing(e);
+					clear();
+				}
+			});
+			
+			setVisible(true);
+		}
+		
+		public void clear() {
+			txfUserName.setText(ConstantsUI.RESTAURANT_NAME);
+			txPassword.setText(ConstantsUI.PASSWORD);
+			txConfirmPasword.setText(ConstantsUI.PASSWORD);
+			ftxfContactNumber.setText(ConstantsUI.PHONE_NUMBER);;
+		}
+		
+}
