f0b49be8744f777d8cdac10de0858106e0705999
joan fonseca <joanfv111@gmail.com>
2016-12-01 12:18:00 -0500

    modificado los parametros de product.

--- softwareDomicile/src/model/dao/ProductManager.java
+++ softwareDomicile/src/model/dao/ProductManager.java
@@ -1,18 +1,19 @@
 package model.dao;
+
 import java.util.ArrayList;
-import javax.swing.ImageIcon;
 import exceptions.ExceptionSearchId;
 import model.entity.Product;
 import model.entity.State;
 
 public class ProductManager {
 	private ArrayList<Product> productList;
-	
-	public ProductManager(){
+
+	public ProductManager() {
 		productList = new ArrayList<>();
 	}
+
 	public static Product createProduct(String name, String description, double price, State state, String img) {
-		return new Product(name, description, price, state,img);
+		return new Product(name, description, price, state, img);
 	}
 
 	public void addProduct(Product product) {
@@ -56,5 +57,4 @@ public class ProductManager {
 			}
 		}
 	}
-
 }
